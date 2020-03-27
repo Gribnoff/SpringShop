71fe9abde97efc8fa85d33c09d84bbbed1a85a42
Gribnoff <quinvex88@gmail.com>
2020-03-27 15:10:24 +0300

    added images upload

--- src/main/java/ru/gribnoff/springshop/services/ImageService.java
+++ src/main/java/ru/gribnoff/springshop/services/ImageService.java
@@ -3,9 +3,11 @@ package ru.gribnoff.springshop.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import ru.gribnoff.springshop.persistence.entities.Image;
 import ru.gribnoff.springshop.persistence.repositories.ImageRepository;
@@ -15,10 +17,11 @@ import javax.imageio.ImageIO;
 
 import java.awt.image.BufferedImage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -28,7 +31,7 @@ import java.util.UUID;
 public class ImageService {
 
     @Value("${files.storepath.images}")
-    private Path IMAGES_PATH;
+    private Path PRODUCT_IMAGES_PATH;
 
     @Value("${files.storepath.icons}")
     private Path ICONS_PATH;
@@ -44,26 +47,35 @@ public class ImageService {
     }
 
     public BufferedImage loadFileAsResource(String id) throws IOException {
-            Resource resource = UUIDValidator.isUUID(id)
-                    ? new UrlResource(IMAGES_PATH
-                        .resolve(getImageNameById(UUID.fromString(id)).getName())
-                        .normalize()
-                        .toUri())
-                    : new UrlResource(ICONS_PATH
-                        .resolve(id)
-                        .normalize()
-                        .toUri());
+        Resource resource = UUIDValidator.isUUID(id)
+                ? new UrlResource(PRODUCT_IMAGES_PATH
+                .resolve(getImageNameById(UUID.fromString(id)).getName())
+                .normalize()
+                .toUri())
+                : new UrlResource(ICONS_PATH
+                .resolve(id)
+                .normalize()
+                .toUri());
 
-            return resource.exists()
-                    ? ImageIO.read(resource.getFile())
-                    : ImageIO.read(new UrlResource(ICONS_PATH
-                        .resolve("image_not_found.png")
-                        .normalize()
-                        .toUri())
-                        .getFile());
+        return resource.exists()
+                ? ImageIO.read(resource.getFile())
+                : ImageIO.read(new UrlResource(ICONS_PATH
+                .resolve("image_not_found.png")
+                .normalize()
+                .toUri())
+                .getFile());
     }
 
-    public Image uploadImage(MultipartFile image, String title) {
-        return null;
+    @Transactional
+    public Image uploadImage(MultipartFile image) throws IOException {
+        String uploadedFileName = image.getOriginalFilename();
+        if (image.isEmpty() || uploadedFileName == null)
+            throw new FileNotFoundException("File not specified");
+        else {
+            Path path;
+            path = Paths.get(PRODUCT_IMAGES_PATH.resolve(uploadedFileName).toUri().normalize());
+            image.transferTo(path);
+        }
+        return imageRepository.save(new Image(uploadedFileName));
     }
 }
