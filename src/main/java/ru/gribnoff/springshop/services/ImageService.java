c4d2a5361a7df82dec009dec04830da37bbd15b3
Gribnoff <quinvex88@gmail.com>
2020-03-30 02:11:17 +0300

    photos on review add + image storing logic changed

--- src/main/java/ru/gribnoff/springshop/services/ImageService.java
+++ src/main/java/ru/gribnoff/springshop/services/ImageService.java
@@ -12,8 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import ru.gribnoff.springshop.exceptions.UnsupportedMediaTypeException;
 import ru.gribnoff.springshop.persistence.entities.Image;
+import ru.gribnoff.springshop.persistence.entities.Product;
+import ru.gribnoff.springshop.persistence.entities.ShopUser;
+import ru.gribnoff.springshop.persistence.entities.enums.ImageCategory;
 import ru.gribnoff.springshop.persistence.repositories.ImageRepository;
-import ru.gribnoff.springshop.util.UUIDValidator;
 
 import javax.imageio.ImageIO;
 
@@ -26,6 +28,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -33,11 +36,17 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ImageService {
 
-    @Value("${files.storepath.images}")
+    @Value("${files.storepath.product-images}")
     private Path PRODUCT_IMAGES_PATH;
 
     @Value("${files.storepath.icons}")
     private Path ICONS_PATH;
+
+    @Value("${files.storepath.review-photos}")
+    private Path REVIEW_PHOTO_PATH;
+
+    @Value("${files.storepath.images-dump}")
+    private Path IMAGES_DUMP;
 
     private final ImageRepository imageRepository;
 
@@ -49,16 +58,28 @@ public class ImageService {
         return imageRepository.findImageById(imageId);
     }
 
-    public BufferedImage loadFileAsResource(String id) throws IOException {
-        Resource resource = UUIDValidator.isUUID(id)
-                ? new UrlResource(PRODUCT_IMAGES_PATH
-                .resolve(getImageNameById(UUID.fromString(id)).getName())
-                .normalize()
-                .toUri())
-                : new UrlResource(ICONS_PATH
-                .resolve(id)
-                .normalize()
-                .toUri());
+    public BufferedImage loadFileAsResource(String id, ImageCategory category) throws IOException {
+        Resource resource = null;
+        switch (category) {
+            case PRODUCT_IMAGE:
+                resource = new UrlResource(PRODUCT_IMAGES_PATH
+                        .resolve(getImageNameById(UUID.fromString(id)).getName())
+                        .normalize()
+                        .toUri());
+                break;
+            case REVIEW_PHOTO:
+                resource = new UrlResource(REVIEW_PHOTO_PATH
+                        .resolve(getImageNameById(UUID.fromString(id)).getName())
+                        .normalize()
+                        .toUri());
+                break;
+            case ICON:
+                resource = new UrlResource(ICONS_PATH
+                        .resolve(id)
+                        .normalize()
+                        .toUri());
+                break;
+        }
 
         return resource.exists()
                 ? ImageIO.read(resource.getFile())
@@ -70,18 +91,53 @@ public class ImageService {
     }
 
     @Transactional
-    public Image uploadImage(MultipartFile image) throws IOException {
+    public Image uploadImage(MultipartFile image, ImageCategory category) throws IOException {
         String uploadedFileName = image.getOriginalFilename();
         if (image.isEmpty() || uploadedFileName == null)
             throw new FileNotFoundException("File not specified");
         else {
             if (isValidImageExtension(image)) {
-                Path path;
-                path = Paths.get(PRODUCT_IMAGES_PATH.resolve(uploadedFileName).toUri().normalize());
+                Path path = Paths.get(getPathByCategory(category).resolve(uploadedFileName).toUri().normalize());
                 image.transferTo(path);
             }
         }
         return imageRepository.save(new Image(uploadedFileName));
+    }
+
+    public Image uploadReviewPhoto(MultipartFile image, Product product, ShopUser shopUser) throws IOException {
+        String fileExtension = getExtensionByStringHandling(image.getOriginalFilename()).get();
+        String uploadedFileName = product.getTitle()
+                + shopUser.getFirstName()
+                + shopUser.getLastName()
+                + "." + fileExtension;
+        if (image.isEmpty())
+            throw new FileNotFoundException("File not specified");
+        else {
+            if (isValidImageExtension(image)) {
+                Path path = Paths.get(getPathByCategory(ImageCategory.REVIEW_PHOTO).resolve(uploadedFileName).toUri().normalize());
+                image.transferTo(path);
+            }
+        }
+        return imageRepository.save(new Image(uploadedFileName));
+    }
+
+    private Path getPathByCategory(ImageCategory category) {
+        switch (category) {
+            case ICON:
+                return ICONS_PATH;
+            case PRODUCT_IMAGE:
+                return PRODUCT_IMAGES_PATH;
+            case REVIEW_PHOTO:
+                return REVIEW_PHOTO_PATH;
+            default:
+                return IMAGES_DUMP;
+        }
+    }
+
+    public Optional<String> getExtensionByStringHandling(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
     }
 
     private boolean isValidImageExtension(MultipartFile image) {
--- src/main/resources/db/migration/V6__review.sql
+++ src/main/resources/db/migration/V6__review.sql
@@ -1,18 +1,24 @@
 create table if not exists reviews
 (
 	id uuid default uuid_generate_v4() not null
-		constraint review_pk
+		constraint reviews_pk
 			primary key,
 	shopuser uuid not null
-		constraint review_shopuser_fk
+		constraint reviews_shopuser_fk
 			references shopusers,
 	product uuid not null
-		constraint review_product_fk
+		constraint reviews_product_fk
 			references products,
 	comment varchar(500) not null
 );
 
 alter table reviews owner to postgres;
 
-create unique index if not exists review_id_uindex
+create unique index if not exists reviews_id_uindex
 	on reviews (id);
+
+alter table reviews
+    add image uuid default null;
+alter table reviews
+    add constraint reviews_image_fk
+        foreign key (image) references images;
