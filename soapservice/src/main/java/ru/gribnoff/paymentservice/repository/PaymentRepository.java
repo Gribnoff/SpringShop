c6eafbe3ae244dabc344d6942de63a2fb0cb5e0c
Gribnoff <quinvex88@gmail.com>
2020-04-01 14:59:12 +0300

    added price to soap request

--- soapservice/src/main/java/ru/gribnoff/paymentservice/repository/PaymentRepository.java
+++ soapservice/src/main/java/ru/gribnoff/paymentservice/repository/PaymentRepository.java
@@ -9,27 +9,31 @@ import java.util.stream.Collectors;
 
 @Component
 public class PaymentRepository {
-    public List<Payment> getPayments(String country) {
+    public List<Payment> getPayments(String country, Double price) {
 
         Payment payment1 = new Payment();
         payment1.setId(1);
         payment1.setName("PayPal");
         payment1.setCountry("USA");
+        payment1.setPrice(price);
 
         Payment payment2 = new Payment();
         payment2.setId(2);
         payment2.setName("GooglePay");
         payment2.setCountry("USA");
+        payment2.setPrice(price);
 
         Payment payment3 = new Payment();
         payment3.setId(3);
         payment3.setName("WebMoney");
         payment3.setCountry("Russia");
+        payment3.setPrice(price);
 
         Payment payment4 = new Payment();
         payment4.setId(4);
         payment4.setName("YandexMoney");
         payment4.setCountry("Russia");
+        payment4.setPrice(price);
 
         return new ArrayList<Payment>() {{
             add(payment1);
